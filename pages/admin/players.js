09e016e4e501cda7b5e68a7753bea879dbf19a3f
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-10-27 21:41:21 -0300

    feat(players): add admin list

--- pages/admin/players.js
+++ pages/admin/players.js
@@ -1,10 +1,60 @@
 import React from 'react';
-import AdminLayout from '../../src/components/layouts/admin';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-export default function Index() {
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { deserialize, serialize } from 'superjson';
+
+import AdminLayout from '../../src/components/layouts/admin';
+import { getPlayers } from '../../src/repositories/players';
+import useLocalization from '../../src/useLocalization';
+
+export default function Index({ players: json }) {
+  const { t } = useTranslation()
+  const { l } = useLocalization()
+  const players = deserialize(json)
+
   return (
     <AdminLayout index='players'>
+      <Typography variant="h5" component="h2">
+        {t('admin.players.title')}
+      </Typography>
+      <TableContainer component={Paper} sx={{ marginTop: 2 }}>
+        <Table aria-label={t('admin.players.table.title')}>
+          <TableHead>
+            <TableRow>
+              <TableCell>{t('admin.players.table.name')}</TableCell>
+              <TableCell>{t('admin.players.table.state')}</TableCell>
+              <TableCell>{t('admin.players.table.konamiId')}</TableCell>
+              <TableCell>{t('admin.players.table.createdAt')}</TableCell>
+              <TableCell>{t('admin.players.table.actions')}</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {players.map((player) => (
+              <TableRow
+                key={player.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">{player.name}</TableCell>
+                <TableCell>{player.state}</TableCell>
+                <TableCell>{player.konami_id}</TableCell>
+                <TableCell>{l(player.created_at)}</TableCell>
+                <TableCell>
+
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </AdminLayout>
   );
 }
@@ -13,6 +63,7 @@ export async function getServerSideProps({ locale }) {
   return {
     props: {
       ...(await serverSideTranslations(locale)),
+      players: serialize(await getPlayers())
     }
   }
 }
