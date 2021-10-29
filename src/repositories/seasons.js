3a6dc6c7b5e4e7b8661ea5bcebfc891e025b7b8b
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-10-28 21:42:43 -0300

    feat(seasons): add repository

---
+++ src/repositories/seasons.js
@@ -0,0 +1,24 @@
+import db from '../database'
+
+export async function getSeasons () {
+  return db('seasons').select('*').orderBy('created_at')
+}
+
+export async function createSeason ({ name }) {
+  const season = {
+    name,
+    created_at: new Date(),
+    updated_at: new Date()
+  }
+
+  const [id] = await db('seasons').insert(season)
+
+  return { id, ...season }
+}
+
+export async function deleteSeason (seasonId) {
+  const rows = await db('seasons').where('id', seasonId).del()
+
+  if (rows === 0) { throw new Error('Not Found') };
+  return true
+}
