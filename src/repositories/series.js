52a4c2efaf575b770af8d3c3c6ea60b8d6114efc
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-10-31 01:22:50 -0300

    feat(series): add get serie action

--- src/repositories/series.js
+++ src/repositories/series.js
@@ -3,3 +3,7 @@ import db from '../database'
 export async function getSeries () {
   return db('series').select('*').orderBy('position')
 }
+
+export async function getSerie (serieId) {
+  return (await db('series').where('id', serieId).limit(1))[0]
+}
