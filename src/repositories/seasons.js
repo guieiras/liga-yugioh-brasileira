9bdaf6fa34e9887c601f6ab7813e7c4d21f2b4c2
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-10-30 23:24:52 -0300

    feat(seasons): add participations methods

--- src/repositories/seasons.js
+++ src/repositories/seasons.js
@@ -25,6 +25,32 @@ export async function createSeason ({ name }) {
 export async function deleteSeason (seasonId) {
   const rows = await db('seasons').where('id', seasonId).del()
 
-  if (rows === 0) { throw new Error('Not Found') };
+  if (rows === 0) { throw new Error('Not Found') }
+  return true
+}
+
+export async function getParticipations (seasonId) {
+  return db('seasons_participations').where('season_id', seasonId)
+}
+
+export async function createSeasonParticipation ({ playerId, seasonId, serieId }) {
+  const participation = {
+    player_id: playerId,
+    season_id: seasonId,
+    serie_id: serieId
+  }
+
+  await db('seasons_participations').insert(participation)
+
+  return participation
+}
+
+export async function deleteSeasonParticipation ({ playerId, seasonId }) {
+  const rows = await db('seasons_participations')
+    .where('season_id', seasonId)
+    .where('player_id', playerId)
+    .del()
+
+  if (rows === 0) { throw new Error('Not Found') }
   return true
 }
