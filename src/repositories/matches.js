7b88f776feb0217067b7f66476b30f0be230a183
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-10-31 12:09:58 -0300

    feat(matches): add repository

---
+++ src/repositories/matches.js
@@ -0,0 +1,15 @@
+import db from '../database'
+
+export async function searchMatches({ seasonId, serieId, round  }) {
+  const query = db('matches')
+  if (seasonId) { query.where('season_id', seasonId) }
+  if (serieId) { query.where('serie_id', serieId) }
+  if (round === 'last') {
+    const lastRound = (await db('matches').max('round', { as: 'round' }))[0].round
+    query.where('round', lastRound || 1)
+  } else if (round) {
+    query.where('round', round)
+  }
+
+  return query
+}
