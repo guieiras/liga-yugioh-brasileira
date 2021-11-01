59367df3e1dc49b3da192861e5df70d8a71dd604
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-11-01 01:23:47 -0300

    feat(matches): add matches on panel

--- src/components/matches/panel.js
+++ src/components/matches/panel.js
@@ -1,19 +1,22 @@
 import React from 'react'
 
 import Box from '@mui/material/Box'
-import Paper from '@mui/material/Paper'
-import List from '@mui/material/List'
 import Button from '@mui/material/Button'
-import IconButton from '@mui/material/IconButton'
+import Grid from '@mui/material/Grid'
+import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import ListItemText from '@mui/material/ListItemText'
+import Paper from '@mui/material/Paper'
+import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
+import AddIcon from '@mui/icons-material/Add'
 import ArrowBackIcon from '@mui/icons-material/ArrowBackIos'
 import ArrowForwardIcon from '@mui/icons-material/ArrowForwardIos'
+import ClearIcon from '@mui/icons-material/Clear'
 import { useTranslation } from 'next-i18next'
 
-export default function MatchesPanel ({ onNewRound, matches, round, lastRound, sx, ...props }) {
+export default function MatchesPanel ({
+  onNewRound, matches, players, onBack, onForward, round, lastRound, sx, ...props
+}) {
   const { t } = useTranslation()
 
   if (round === 0) {
@@ -28,11 +31,35 @@ export default function MatchesPanel ({ onNewRound, matches, round, lastRound, s
   return (
     <Paper {...props} sx={{ p: 3, ...(sx || {}) }}>
       <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-        { round > 1 ? <ArrowBackIcon /> : <span /> }
+        <IconButton disabled={round === 1} onClick={onBack}><ArrowBackIcon /></IconButton>
         <Typography variant="button">{t('currentRound', { round })}</Typography>
-        <ArrowForwardIcon />
+        {
+          round < lastRound ? (
+            <IconButton onClick={onForward}><ArrowForwardIcon /></IconButton>
+          ) : (
+            <IconButton onClick={onNewRound}><AddIcon /></IconButton>
+          )
+        }
       </Box>
+
       <List>
+        {
+          matches.map((match) => (
+            <ListItem key={match.id}>
+              <Grid container>
+                <Grid item xs={5} sx={{ textAlign: 'right' }}>
+                  <Typography sx={{ p: 1 }}>{players[match.home_player_id]}</Typography>
+                </Grid>
+                <Grid item xs={2} sx={{ textAlign: 'center' }}>
+                  <ClearIcon sx={{ height: '100%' }} />
+                </Grid>
+                <Grid item xs={5} sx={{ textAlign: 'left' }}>
+                  <Typography sx={{ p: 1 }}>{players[match.away_player_id]}</Typography>
+                </Grid>
+              </Grid>
+            </ListItem>
+          ))
+        }
       </List>
     </Paper>
   )
