53333ca2f33813289d0fd9b603c6c86051109205
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-11-02 19:14:29 -0300

    feat(seasons): add visibility fields

---
+++ migrations/20211102125136_add_visibility_fields_to_seasons.js
@@ -0,0 +1,20 @@
+exports.up = async function(knex) {
+  await knex.schema.table('seasons', function (table) {
+    table.boolean('current')
+    table.boolean('hidden')
+  })
+
+  await knex('seasons').update({ current: false, hidden: false })
+
+  await knex.schema.table('seasons', function (table) {
+    table.boolean('current').notNullable().alter()
+    table.boolean('hidden').notNullable().alter()
+  })
+}
+
+exports.down = async function(knex) {
+  await knex.schema.table('seasons', function (table) {
+    table.dropColumn('current')
+    table.dropColumn('hidden')
+  })
+}
