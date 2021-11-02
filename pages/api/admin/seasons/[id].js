3a300c088f62c610cb2e712ccb81566e98fb0502
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-11-02 19:22:00 -0300

    feat(seasons): add update action on api

--- pages/api/admin/seasons/[id].js
+++ pages/api/admin/seasons/[id].js
@@ -1,8 +1,9 @@
 import withAuthentication from '../../../../src/middlewares/withAuthentication'
-import { deleteSeason } from '../../../../src/repositories/seasons'
+import { deleteSeason, updateSeason } from '../../../../src/repositories/seasons'
 
 export default withAuthentication((req, res) => {
   if (req.method === 'DELETE') { return destroy(req, res) }
+  if (req.method === 'PUT') { return update(req, res) }
 
   return res.status(404).send({ error: 'Route not found' })
 })
@@ -11,6 +12,18 @@ async function destroy (req, res) {
   try {
     await deleteSeason(req.query.id)
     res.status(204).send('')
+  } catch {
+    res.status(404).json({ error: 'Not Found' })
+  }
+}
+
+async function update (req, res) {
+  try {
+    const permittedParams = { current: req.body.current }
+    const season = await updateSeason(req.query.id, permittedParams)
+    if (!season) { throw new Error() }
+
+    res.status(200).send(season)
   } catch {
     res.status(404).json({ error: 'Not Found' })
   }
