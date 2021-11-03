fe1cc9fc45c45e0c5acd359527c98b19e43ed60e
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-11-03 19:47:25 -0300

    feat(players): add search by participation

--- src/repositories/players.js
+++ src/repositories/players.js
@@ -13,6 +13,22 @@ export async function searchPlayers ({ name, id, nid }) {
   return query
 }
 
+export async function searchPlayersByParticipation (participations) {
+  const query =
+    db('players')
+      .join('seasons_participations', 'seasons_participations.player_id', 'players.id')
+      .distinct('players.id', 'players.name', 'players.state')
+
+  participations.forEach(participation => (
+    query.orWhere({
+      season_id: participation.season_id,
+      serie_id: participation.serie_id
+    })
+  ))
+
+  return query
+}
+
 export async function createPlayer ({ name, state, konamiId }) {
   const player = {
     name,
