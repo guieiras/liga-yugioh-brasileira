820c93e95de358fb155c071eb22333998a36cdf7
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-11-03 19:44:27 -0300

    lint: fix migration

--- migrations/20211102125136_add_visibility_fields_to_seasons.js
+++ migrations/20211102125136_add_visibility_fields_to_seasons.js
@@ -1,4 +1,4 @@
-exports.up = async function(knex) {
+exports.up = async function (knex) {
   await knex.schema.table('seasons', function (table) {
     table.boolean('current')
     table.boolean('hidden')
@@ -12,7 +12,7 @@ exports.up = async function(knex) {
   })
 }
 
-exports.down = async function(knex) {
+exports.down = async function (knex) {
   await knex.schema.table('seasons', function (table) {
     table.dropColumn('current')
     table.dropColumn('hidden')
