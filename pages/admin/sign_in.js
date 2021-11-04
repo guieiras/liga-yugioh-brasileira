18a2ad513d9be20366ce0b7573c04ba4242a6ee1
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-11-03 22:58:53 -0300

    feat(admin): add title to sign in

--- pages/admin/sign_in.js
+++ pages/admin/sign_in.js
@@ -1,12 +1,16 @@
 import Container from '@mui/material/Container'
 import CircularProgress from '@mui/material/CircularProgress'
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { signIn, useSession } from 'next-auth/react'
+import Head from 'next/head'
 import { useRouter } from 'next/router'
 
 export default function AdminSignIn () {
+  const { t } = useTranslation()
   const { data: session, status } = useSession()
   const loading = status === 'loading'
-  const { push } = useRouter()
+  const { replace } = useRouter()
 
   const login = () => {
     signIn('auth0')
@@ -15,25 +19,37 @@ export default function AdminSignIn () {
   }
 
   const redirectToDashboard = () => {
-    push('/admin/players')
-
+    replace('/admin')
     return null
   }
 
-  if (loading || !session) {
-    return <Container align='center' sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      height: '100vh',
-      justifyContent: 'center',
-      textAlign: 'center'
-    }}>
+  return <>
+    <Head>
+      <title>{t('admin.home')}</title>
+    </Head>
+
+    { (loading || !session) && <Container align='center' sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+        justifyContent: 'center',
+        textAlign: 'center'
+      }}>
         <div>
           { (!loading && !session) ? login() : null }
           <CircularProgress />
         </div>
-    </Container>
-  }
+    </Container> }
 
-  if (session) { return redirectToDashboard() }
+    { session && redirectToDashboard() }
+  </>
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      locale,
+      ...(await serverSideTranslations(locale))
+    }
+  }
 }
