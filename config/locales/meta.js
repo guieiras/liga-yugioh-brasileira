5f9be18e3984c6a3a78583362a8ba6ec401c2686
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-11-07 11:47:58 -0300

    chore: add meta and logos

---
+++ config/locales/meta.js
@@ -0,0 +1,10 @@
+const meta = {
+  pt: {
+    description: 'A Liga Yu-Gi-Oh! Brasileira é um torneio criado para fortalecer o cenário competitivo do jogo no país.'
+  },
+  en: {
+    description: 'Yu-Gi-Oh! Brazilian League is a tournament to improve the competitive environment on Brazil.'
+  }
+}
+
+export default meta
--- pages/_document.js
+++ pages/_document.js
@@ -3,17 +3,29 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 import createEmotionServer from '@emotion/server/create-instance'
 import theme from '../src/theme'
 import createEmotionCache from '../src/createEmotionCache'
+import meta from '../config/locales/meta'
 
 export default class AppDocument extends Document {
   render () {
     return (
-      <Html lang="en">
+      <Html lang={this.props.locale}>
         <Head>
           <meta name="theme-color" content={theme.palette.primary.main} />
+          <meta name="description" content={meta[this.props.locale].description} />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+          <link rel="manifest" href={`/manifest-${this.props.locale}.json`} />
         </Head>
         <body>
           <Main />
@@ -50,6 +62,7 @@ AppDocument.getInitialProps = async (ctx) => {
 
   return {
     ...initialProps,
-    styles: [...React.Children.toArray(initialProps.styles), ...emotionStyleTags]
+    styles: [...React.Children.toArray(initialProps.styles), ...emotionStyleTags],
+    locale: ctx.locale
   }
 }
