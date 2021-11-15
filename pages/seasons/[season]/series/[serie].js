4dfa0467f16b6f133214a4ed868071047867517b
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-11-14 23:34:49 -0300

    feat(series): add playoffs on public pages

--- pages/seasons/[season]/series/[serie].js
+++ pages/seasons/[season]/series/[serie].js
@@ -5,7 +5,9 @@ import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 import { useTranslation } from 'next-i18next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { ROUNDS } from '../../../../src/components/playoffs/constants'
 import PublicLayout from '../../../../src/components/layouts/public'
+import PlayoffsPanel from '../../../../src/components/playoffs/panel'
 import RoundsPanel from '../../../../src/components/rounds/panel'
 import { get, post } from '../../../../src/requests/client'
 import generateStandings from '../../../../src/helpers/standings'
@@ -16,18 +18,21 @@ export default function SeasonSerieShow ({ locale, seasonSlug, serieSlug }) {
   const [season, setSeason] = React.useState(null)
   const [serie, setSerie] = React.useState(null)
   const [roundMatches, setRoundMatches] = React.useState({})
+  const [playoffMatches, setPlayoffMatches] = React.useState({})
   const [players, setPlayers] = React.useState({})
   const [playerNames, setPlayerNames] = React.useState({})
   const [currentRound, setCurrentRound] = React.useState(0)
   const [lastRound, setLastRound] = React.useState(null)
   const [standings, setStandings] = React.useState(null)
   const [breakpoints, setBreakpoints] = React.useState([])
+  const [currentStep, setCurrentStep] = React.useState(null)
+  const [playoffSteps, setPlayoffSteps] = React.useState([])
 
   React.useEffect(getSeasonAndSerie, [])
   React.useEffect(() => {
     if (serie) {
       Promise.all(
-        [getParticipations(), getRound(), getSwissStandings(), getBreakpoints()]
+        [getParticipations(), getRound(), getSwissStandings(), getBreakpoints(), getPlayoffSteps()]
       )
     }
   }, [serie])
@@ -62,6 +67,39 @@ export default function SeasonSerieShow ({ locale, seasonSlug, serieSlug }) {
     }
   }
 
+  async function getPlayoffSteps () {
+    const playoffs = await get('playoffs', { season_id: season.id, serie_id: serie.id })
+    const steps = ROUNDS.filter(round => round.steps.find(step => playoffs.includes(step.index)))
+    setPlayoffSteps(steps.map(step => ({ ...step, loaded: false })))
+
+    if (steps.length) {
+      await getPlayoffMatches(steps[0], steps)
+      setCurrentStep(0)
+    } else {
+      setCurrentStep(null)
+    }
+  }
+
+  async function getPlayoffMatches (round, baseSteps) {
+    if (round.loaded) return
+
+    const steps = round.steps
+    const matches = steps.reduce((memo, step) => ({ ...memo, [step.index]: [] }), { ...playoffMatches })
+    setPlayoffMatches(matches)
+
+    const results = await get('matches', {
+      season_id: season.id, serie_id: serie.id, playoffs: steps.map(step => step.index)
+    })
+
+    setPlayoffSteps(
+      (baseSteps || playoffSteps)
+        .map((step) => step.name === round.name ? { ...step, loaded: true } : step)
+    )
+    setPlayoffMatches(results.reduce((memo, match) => (
+      { ...memo, [match.playoff]: [...(memo[match.playoff] || []), match] }
+    ), matches))
+  }
+
   async function getSwissStandings () {
     const table = await get('standings', { season_id: season.id, serie_id: serie.id })
     setStandings(generateStandings(table))
@@ -71,14 +109,14 @@ export default function SeasonSerieShow ({ locale, seasonSlug, serieSlug }) {
     setBreakpoints(await get(`seasons/${season.id}/series/${serie.id}/breakpoints`))
   }
 
-  async function handleBack () {
-    setCurrentRound(currentRound - 1)
-    await getRound(currentRound - 1)
+  async function handleRound () {
+    setCurrentRound(currentRound + this)
+    await getRound(currentRound + this)
   }
 
-  async function handleForward () {
-    setCurrentRound(currentRound + 1)
-    await getRound(currentRound + 1)
+  async function handlePlayoff () {
+    setCurrentStep(currentStep + this)
+    await getPlayoffMatches(playoffSteps[currentStep + this])
   }
 
   return (
@@ -104,11 +142,24 @@ export default function SeasonSerieShow ({ locale, seasonSlug, serieSlug }) {
               loading={!roundMatches[currentRound]}
               linkSize="small"
               matches={roundMatches[currentRound] || []}
-              onBack={handleBack}
-              onForward={handleForward}
+              onBack={handleRound.bind(-1)}
+              onForward={handleRound.bind(1)}
               players={playerNames}
               round={currentRound}
             />
+            <PlayoffsPanel
+              currentStep={playoffSteps[currentStep]}
+              lastStep={playoffSteps.length - 1}
+              matches={Object.fromEntries(
+                (playoffSteps[currentStep]?.steps || []).map(step => (
+                  [step.index, playoffMatches[step.index]]
+                ), [])
+              ) }
+              onBack={handlePlayoff.bind(-1)}
+              onForward={handlePlayoff.bind(1)}
+              players={playerNames}
+              stepIndex={currentStep}
+              sx={{ mt: 2 }} />
           </Grid>
         </Grid>
       </>
