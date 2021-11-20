026e3b83a9b639494c666e015a2c7c365fcbd1de
Guilherme Eiras <guialbertoeiras@hotmail.com>
2021-11-20 15:25:04 -0300

    feat(playoffs): add games count

--- src/components/playoffs/constants.js
+++ src/components/playoffs/constants.js
@@ -2,34 +2,34 @@ export const ROUNDS = [
   {
     name: 'top32',
     steps: [
-      { name: 'top32', index: 32 }
+      { name: 'top32', index: 32, games: 16 }
     ]
   },
   {
     name: 'top16',
     steps: [
-      { name: 'top16', index: 16 }
+      { name: 'top16', index: 16, games: 8 }
     ]
   },
   {
     name: 'top8',
     steps: [
-      { name: 'top8', index: 8 }
+      { name: 'top8', index: 8, games: 4 }
     ]
   },
   {
     name: 'qualifiers',
     steps: [
-      { name: 'top4', index: 4 },
-      { name: 'recap', index: 7 }
+      { name: 'top4', index: 4, games: 2 },
+      { name: 'recap', index: 7, games: 1 }
     ]
   },
   {
     name: 'finals',
     steps: [
-      { name: 'final', index: 1 },
-      { name: 'third', index: 3 },
-      { name: 'promotion', index: 5 }
+      { name: 'promotion', index: 5, games: 1 },
+      { name: 'third', index: 3, games: 1 },
+      { name: 'final', index: 1, games: 1 }
     ]
   }
 ]
